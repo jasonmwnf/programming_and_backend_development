5e721a452542ddacca7f7b1aa1c758b7ce284476
Jason Williams <jasonmw_nf@hotmail.com>
2018-09-23 20:07:51 -0230

    added solution for stringy string.

---
+++ small_problems/stringy_strings.rb
@@ -0,0 +1,14 @@
+# Write a method that takes one argument, a positive integer, and returns a string of alternating 1s and 0s, always starting with 1. The length of the string should match the given integer.
+
+def stringy(num)
+  str = ''
+  (1..num).each do |n|
+    n.odd? ? str << "1" : str << "0"
+  end
+  str
+end
+
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
